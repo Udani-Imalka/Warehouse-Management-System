cb2f52c4267dbc4bd6d2c0ff6e058d4bb8267d63
nethmi1234 <nethmiwarnakula@gmail.com>
2022-02-12 15:35:37 +0530

    add button component

--- warehouse-frontend/src/App.js
+++ warehouse-frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Button from './Components/Button';
+import Table from './Components/Table';
 
 function App() {
   return (
     <div className="App">
       <Button text="Confirm"></Button>
+      <Table header1="Name"></Table>
     </div>
   );
 }
