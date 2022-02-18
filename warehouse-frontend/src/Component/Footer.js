951b76af3c4d7c74723305909885d7496f65fea2
Udani Imalka <uimalka.96@gmail.com>
2022-02-18 07:30:36 +0530

    add the home file

--- warehouse-frontend/src/Component/Footer.js
+++ warehouse-frontend/src/Component/Footer.js
@@ -1,4 +1,22 @@
-import React from 'react'
+import React, { Component } from "react";
 
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6">© Skote.</div>
+            <div className="col-sm-6">
+              <div className="text-sm-end d-none d-sm-block">
+                Design &amp; Develop by Themesbrand
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
 
-
+export default Footer;
